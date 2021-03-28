ec7005c5d353c4d3d783155cd3f2c754511213af
Felipe Stahlhofer <43865622+FelipeStahl@users.noreply.github.com>
2021-03-28 18:30:20 -0300

    Inicial

---
+++ Md5string.test/Md5stringTeste.cs
@@ -0,0 +1,20 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Md5string.test
+{
+    [TestClass]
+    public class Md5stringTeste
+    {
+        [TestCategory("MD5 Testes")]
+        [TestMethod("Shold validate MD5 string")]
+        [DataRow(null, false)]
+        [DataRow(" ", false)]
+        [DataRow("testando", true)]
+        //CAA9C8F8620CBB30679026BB6427E11F == testando
+        public void SholdValidateMd5string(string value, bool status)
+        {
+            var result = "CAA9C8F8620CBB30679026BB6427E11F";
+            Assert.AreEqual(value.ToMd5() == result, status);
+        }
+    }
+}
---
+++ Md5string/Md5string.cs
@@ -0,0 +1,23 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace Md5string
+{
+    public static class Md5string
+    {
+        public static string ToMd5(this string value){
+            if(string.IsNullOrEmpty(value)){
+                return string.Empty;
+            }
+            using var md5 = MD5.Create();
+            var inputBytes = Encoding.ASCII.GetBytes(value);
+            var hashBytes = md5.ComputeHash(inputBytes);
+            var sb = new StringBuilder();
+            foreach (var item in hashBytes)
+            {
+                sb.Append(item.ToString("X2"));   
+            }
+            return sb.ToString();
+        }
+    }
+}
